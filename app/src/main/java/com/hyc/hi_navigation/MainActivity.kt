4cea51e823ee732c90c795575025f466daa3036d
dt_6010331431 <huyongchao@zhijianzhuoyue.com>
2024-12-27 17:40:28 +0800

    避免Fragment 每次切换时都重新走一遍生命周期

--- app/src/main/java/com/hyc/hi_navigation/MainActivity.kt
+++ app/src/main/java/com/hyc/hi_navigation/MainActivity.kt
@@ -3,7 +3,6 @@ package com.hyc.hi_navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hyc.hi_navigation.utlis.NavUtlis
 
